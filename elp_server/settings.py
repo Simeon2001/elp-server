a9e4e062b922b44410345de5da6d340a81faeb54
Simeon2001 <jesusanyasimeon@gmail.com>
2022-09-10 15:52:23 +0100

    uncomment some codes

--- elp_server/settings.py
+++ elp_server/settings.py
@@ -92,11 +92,11 @@ DATABASES = {
     }
 }
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-#DATABASES = {
-#    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-#}
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
 
 
 # Password validation
