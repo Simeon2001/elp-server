c215f7afe68eb314bc492085b6e61764990c7c56
Simeon2001 <jesusanyasimeon@gmail.com>
2022-10-28 11:55:17 +0100

    refix

--- elp_server/settings.py
+++ elp_server/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv("HOST"), "9f20-102-89-46-199.ngrok.io", "elp-simeon2001.koyeb.app"]
+ALLOWED_HOSTS = [os.getenv("HOST"), "9f20-102-89-46-199.ngrok.io", "elp-simeon2001.koyeb.app", "0.0.0.0"]
 
 
 # Application definition
